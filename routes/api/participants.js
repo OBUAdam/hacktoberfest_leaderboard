fbe8802be1f078d1389cea3315e7abf1ca85f208
Austin W Keeton <austin.w.keeton@gmail.com>
2018-10-06 12:43:39 -0500

    Merged functionality of scores.js into participants.js

--- routes/api/participants.js
+++ routes/api/participants.js
@@ -7,9 +7,30 @@ const router = express.Router();
 // @desc    Get all participants
 // @access  Public
 router.get('/', (req, res) => {
-  axios
-    .get('https://raw.githubusercontent.com/austinwk/2018_hacktoberfest_participants/master/participants.json')
-    .then(res => console.log(res.data));
+  axios("https://raw.githubusercontent.com/austinwk/2018_hacktoberfest_participants/master/participants.json")
+    .then(participants => {
+      let requests = [];
+
+      participants.data.forEach(participant => {
+        const { githubUserName } = participant;
+        let request = axios(`https://api.github.com/search/issues?q=author:${githubUserName}+type:pr+created:>=2018-10-01T00:00:00`)
+          .then(gitHubStats => gitHubStats.data);
+
+        requests.push(request);
+      });
+
+      Promise.all(requests)
+        .then(gitHubStats => {
+          const profiles = gitHubStats.map(stats => {
+            const numPullReq = stats.total_count;
+
+            return { numPullReq };
+          });
+
+          res.send(profiles);
+        })
+    })
+    .catch(error => console.log(error));
 });
 
 module.exports = router;
