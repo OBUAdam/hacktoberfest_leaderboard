c385ae649cf52dd64af35175545356f007ee8f9a
Austin W Keeton <austin.w.keeton@gmail.com>
2018-10-11 09:39:50 -0500

    Complete rewrite: connect to server, use different routes, use env variables

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import path from 'path';
 
-const participants = require('./routes/api/participants');
+import routes from './server/src/routes/participantRoutes';
 
+
+// If not in production, set environment variables from ./.env
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+  process.env.PORT = 5000;
+}
+
+// Get environment variables
+const {
+  DB_NAME,
+  DB_PORT,
+  DB_HOST,
+  DB_USERNAME,
+  DB_PASSWORD,
+  PORT
+} = process.env;
+
+// Connect to the database
+const DB_CONNECTION_STRING = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
+mongoose
+  .connect(DB_CONNECTION_STRING, { useNewUrlParser: true })
+  .then(() => console.log(`Connected to database: ${DB_NAME}`))
+  .catch(err => console.log(err));
+
+// Start server
 const app = express();
-
-// Bodyparser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// Use Routes
-app.use('/api/participants', participants);
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
+  // Set static folder
+  app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
-const port = process.env.PORT || 5000;
+routes(app);
 
-app.listen(port, () => console.log(`Server started on ${port}`));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
