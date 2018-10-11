631b6eb0431f171ada3b712069f619e4585194c5
Austin W Keeton <austin.w.keeton@gmail.com>
2018-10-11 09:42:07 -0500

    Prepare route for accessing github api

---
+++ server/src/routes/githubRouter.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const axios = require('axios');
+
+const router = express.Router();
+
+// @route   GET api/participants
+// @desc    Get all participants
+// @access  Public
+router.get('/', (req, res) => {
+  axios("https://raw.githubusercontent.com/austinwk/2018_hacktoberfest_participants/master/participants.json")
+    .then(participants => {
+      let requests = [];
+
+      participants.data.forEach(participant => {
+        const { githubUserName } = participant;
+        let request = axios(`https://api.github.com/search/issues?q=author:${githubUserName}+type:pr+created:>=2018-10-01T00:00:00`)
+          .then(gitHubStats => gitHubStats.data);
+
+        requests.push(request);
+      });
+
+      Promise.all(requests)
+        .then(gitHubStats => {
+          const profiles = gitHubStats.map((stats, index) => {
+            const { githubUserName, displayName } = participants.data[index];
+            const numPullReq = stats.total_count;
+
+            return { displayName, githubUserName, numPullReq };
+          });
+
+          res.send(profiles);
+        })
+    })
+    .catch(error => console.log(error));
+});
+
+module.exports = router;
