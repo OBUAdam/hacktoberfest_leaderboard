1a893790d052c2864df26c7e2c7f519d5cb29996
Austin W Keeton <austin.w.keeton@gmail.com>
2018-10-12 08:06:53 -0500

    Change api path

--- client/src/actions/leaderboardActions.js
+++ client/src/actions/leaderboardActions.js
@@ -3,7 +3,7 @@ import { GET_PARTICIPANTS, TOGGLE_LEADERBOARD_LOADING } from './types';
 
 export const getParticipants = () => dispatch => {
   axios
-    .get('/participant')
+    .get('/api/participants')
     .then(res => {
       console.log(res);
       const sorted = res.data.sort((a, b) => b.numPullReq - a.numPullReq);
