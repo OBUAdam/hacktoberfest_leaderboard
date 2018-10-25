6e8d1e49e48fe807ea79b892346b508570882194
Austin W Keeton <austin.w.keeton@gmail.com>
2018-10-25 10:30:44 -0500

    Add sorting and ranking methods.

--- client/src/components/Leaderboard.js
+++ client/src/components/Leaderboard.js
@@ -1,15 +1,56 @@
 import React, { Component } from 'react';
 import { Table } from 'reactstrap';
 import { connect } from 'react-redux';
-import { getParticipants, toggleLeaderboardLoading } from '../actions/leaderboardActions';
+
+import trophy from '../assets/trophy.ico';
+import { getParticipants } from '../actions/leaderboardActions';
 
 class Leaderboard extends Component {
   componentDidMount() {
     this.props.getParticipants();
   }
 
+  sortParticipants = participants => {
+    return participants.sort((a, b) => b.numPullReq - a.numPullReq);
+  };
+
+  rankParticipants = participants => {
+    // If there are no participants, return an empty array
+    if (participants.length === 0) { return []; }
+
+    let rank = 0;
+
+    for (let i = 0; i < participants.length; i++) {
+      if (participants[i].numPullReq >= 5) {
+        participants[i].rank = rank;
+      }
+      else if (i === 0) {
+        rank++;
+        participants[i].rank = rank;
+      }
+      else if (participants[i].numPullReq === participants[i - 1].numPullReq) {
+        participants[i].rank = rank;
+      }
+      else {
+        rank++;
+        participants[i].rank = rank;
+      }
+    };
+
+    return participants;
+  };
+
+  getRank = rank => {
+    if (rank === 0) {
+      return <img src={trophy} alt={rank}/>;
+    }
+    return rank;
+  };
+
   render() {
     const participants = this.props.participants;
+    const sorted = this.sortParticipants(participants);
+    const ranked = this.rankParticipants(sorted);
 
     return (
       <Table responsive={true}>
@@ -22,9 +63,9 @@ class Leaderboard extends Component {
           </tr>
         </thead>
         <tbody>
-          {participants.map((participant, index) =>(
-            <tr key={index}>
-              <th scope="row">{index + 1}</th>
+          {ranked.map((participant) =>(
+            <tr key={participant.githubUserName}>
+              <th scope="row">{this.getRank(participant.rank)}</th>
               <td>{participant.displayName}</td>
               <td>
                 <a href={`https://github.com/${participant.githubUserName}`}>
@@ -45,10 +86,4 @@ const mapStateToProps = (state) => ({
   isLoading: state.leaderboard.isloading
 })
 
-export default connect(
-  mapStateToProps,
-  {
-    getParticipants,
-    toggleLeaderboardLoading
-  }
-)(Leaderboard);
+export default connect(mapStateToProps, { getParticipants })(Leaderboard);
